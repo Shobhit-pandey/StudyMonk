561105b68ae62af4f0d5fa86c03aa86190257f9b
Shobhit Pandey <Shobhitpandey8@gmail.com>
2017-10-24 19:22:19 +0530

    minor change in form

--- mywebsite/form.py
+++ mywebsite/form.py
@@ -5,17 +5,18 @@ from mywebsite.models import StudentRegistration, FacultyRegistration
 
 CHOICE = (
     ('male', 'male'),
-    ('female', 'female')
+    ('female', 'female'),
+    ('other','other')
 )
 
 class StudentRegistrationForm(forms.Form):
 
-    username = forms.CharField(max_length=50)
     first_name = forms.CharField(max_length=50)
     last_name = forms.CharField(max_length=50)
     email = forms.EmailField()
     gender = forms.ChoiceField(CHOICE)
     college_name = forms.CharField(max_length=100)
+    username = forms.CharField(max_length=50)
     password = forms.CharField(max_length=100)
 
     def clean_email(self):
@@ -32,15 +33,16 @@ class StudentRegistrationForm(forms.Form):
 
 
 class FacultyRegistrationForm(forms.Form):
-    username = forms.CharField(max_length=50)
     first_name = forms.CharField(max_length=50)
     last_name = forms.CharField(max_length=50)
     email = forms.EmailField()
     gender = forms.ChoiceField(CHOICE)
     college_name = forms.CharField(max_length=100)
-    password = forms.CharField(max_length=100)
     description = forms.CharField(max_length=1000)
     mentorship_status = forms.BooleanField()
+    username = forms.CharField(max_length=50)
+    password = forms.CharField(max_length=100)
+
 
     def save(self):
         pass
