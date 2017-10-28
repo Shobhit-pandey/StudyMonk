27aec0b52567f3b08cecbeb6171683a99591f7ab
Shobhit Pandey <Shobhitpandey8@gmail.com>
2017-10-28 17:08:19 +0530

    email verification complete

--- mywebsite/form.py
+++ mywebsite/form.py
@@ -14,7 +14,7 @@ class StudentRegistrationForm(forms.Form):
     #TODO provide required attribute
     first_name = forms.CharField(max_length=50,required=True)
     last_name = forms.CharField(max_length=50,required=True)
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(required=True,help_text='Required. Inform a valid email address.')
     gender = forms.ChoiceField(CHOICE,required=True)
     college_name = forms.CharField(max_length=100,required=True)
     username = forms.CharField(max_length=50,required=True)
@@ -32,16 +32,20 @@ class StudentRegistrationForm(forms.Form):
 
         return self.cleaned_data.get('username')
 
-    def save(self):
+    def save(self, kwargs=None):
         print(self.cleaned_data)
-        u = User.objects.create_user(first_name=self.cleaned_data.get('first_name'),last_name=self.cleaned_data.get('last_name'),email=self.cleaned_data.get('email')
-                                ,password = self.cleaned_data.get('password'),username = self.cleaned_data.get('username'))
+        u = User.objects.create_user(first_name=self.cleaned_data.get('first_name'),
+                                     last_name=self.cleaned_data.get('last_name'),
+                                     email=self.cleaned_data.get('email'),
+                                     password = self.cleaned_data.get('password'),
+                                     username = self.cleaned_data.get('username'))
         u.save()
+
         s = StudentRegistration.objects.create(user=u,
                                                gender=self.cleaned_data.get('gender'),
                                                college_name = self.cleaned_data.get('college_name'))
         s.save()
-
+        return u
     def clean_gender(self):
         print("clean gender: ", self.cleaned_data)
         return self.cleaned_data.get('gender')
@@ -52,7 +56,7 @@ class FacultyRegistrationForm(forms.Form):
     #TODO provide required attribute
     first_name = forms.CharField(max_length=50,required=True)
     last_name = forms.CharField(max_length=50,required=True)
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(required=True,help_text='Required. Inform a valid email address.')
     gender = forms.ChoiceField(CHOICE,required=True)
     college_name = forms.CharField(max_length=100,required=True)
     username = forms.CharField(max_length=50,required=True)
@@ -60,7 +64,7 @@ class FacultyRegistrationForm(forms.Form):
     description = forms.CharField(max_length=1000,required=False)
     mentorship_status = forms.BooleanField(required=False)
 
-    def save(self):
+    def save(self, commit=False):
         print(self.cleaned_data)
         u = User.objects.create_user(first_name=self.cleaned_data.get('first_name'),
                                 last_name=self.cleaned_data.get('last_name'),
@@ -75,7 +79,7 @@ class FacultyRegistrationForm(forms.Form):
                                                mentorship_status = self.cleaned_data.get('mentorship_status'),
                                                )
         s.save()
-
+        return u
     def clean_email(self):
         if User.objects.filter(email=self.cleaned_data.get('email', None)).count() > 0:
             raise forms.ValidationError("User with this email already exists")
