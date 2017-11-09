49c6780f95204e70a0c312cb65e1efc7cf63670b
Shobhit Pandey <Shobhitpandey8@gmail.com>
2017-11-09 17:34:22 +0530

    namespace added

--- StudyMonk/urls.py
+++ StudyMonk/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'^', include('mywebsite.urls')),
+    url(r'^', include('mywebsite.urls',namespace='mywebsite')),
 ]
